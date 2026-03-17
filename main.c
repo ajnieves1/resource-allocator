e170d85f899c4693434934232f7a572d04d020b5
ajnieves1 <tun29995@temple.edu>
2026-03-17 19:49:41 -0400

    Created changeGovernor function

--- main.c
+++ main.c
@@ -5,6 +5,19 @@
 
 #define pathToGovernor "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 
+// Pass a pointer of type const char of the mode desired
+void changeGovernor(const char *mode) {
+    // Create file pointer with the path to the governor file and the ability to write
+    FILE *file = fopen(pathToGovernor, "W")
+    if (file == NULL) {
+        perror("Error opening governor path. Make sure you are root.");
+        return;
+    }
+    fprintf(file, "%s", mode);
+    fclose(file);
+    printf("System mode has been set to: %s\n", mode);
+}
+
 int isProcessRunning(const char *nameOfProcess) {
     // Use 'pgrep' via a pipe
     char command[128];
@@ -14,6 +27,7 @@ int isProcessRunning(const char *nameOfProcess) {
 }
 
 int main() {
-    printf("Starting Selective Resource Governor");
+    printf("Starting Selective Resource Manager");
+
     return 0;
 }
