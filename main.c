27660160ee04486208bada60522619c064dfee6e
ajnieves1 <tun29995@temple.edu>
2026-03-17 19:33:23 -0400

    Created isProcessRunning function

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 #define pathToGovernor "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 
+int isProcessRunning(const char *nameOfProcess) {
+    // Use 'pgrep' via a pipe
+    char command[128];
+    sprintf(command, "pgrep %s > /dev/null", nameOfProcess);
+
+    return (system(command)== 0); // Return 1 if found
+}
+
 int main() {
-    printf("Test");
+    printf("Starting Selective Resource Governor");
     return 0;
 }
