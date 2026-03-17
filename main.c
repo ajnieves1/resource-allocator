28c13c679b60c38e6f20f040eed0f4b6f553ddb5
ajnieves1 <tun29995@temple.edu>
2026-03-17 19:25:33 -0400

    Added header files and defined path to governor

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define pathToGovernor "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 
 int main() {
     printf("Test");
